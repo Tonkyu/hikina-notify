53470682ab7fd31450944fcb6940d02932d0f277
Atsuhisa Nakane <tontonkyu@gmail.com>
2024-03-22 18:01:59 +0900

    change template from jpeg to png

--- app/util/create_image.py
+++ app/util/create_image.py
@@ -13,7 +13,7 @@ def create_image(data):
     end_hour = end_datetime.strftime('%H')
     end_minute = end_datetime.strftime('%M')
 
-    temp_img_path = './static/img/template.png'
+    temp_img_path = './static/img/template.jpg'
     img = Image.open(temp_img_path)
 
     w, h = img.size
@@ -56,6 +56,36 @@ def create_image(data):
     draw.text(time_coord, time_text, font=time_font, fill=color, stroke_width=2, stroke_fill="white")
     draw.text(title_coord, title_text, font=title_font, fill=color)
     draw.text(location_coord, location_text, font=location_font, fill=color)
+
+    def draw_rounded_rectangle(draw, xy, rad, fill):
+        x0, y0, x1, y1 = xy
+        draw.rectangle([x0, y0 + rad, x1, y1 - rad], fill=fill)
+        draw.rectangle([x0 + rad, y0, x1 - rad, y1], fill=fill)
+        draw.pieslice([x0, y0, x0 + rad * 2, y0 + rad * 2], 180, 270, fill=fill)
+        draw.pieslice([x1 - rad * 2, y1 - rad * 2, x1, y1], 0, 90, fill=fill)
+        draw.pieslice([x0, y1 - rad * 2, x0 + rad * 2, y1], 90, 180, fill=fill)
+        draw.pieslice([x1 - rad * 2, y0, x1, y0 + rad * 2], 270, 360, fill=fill)
+
+    # 角丸四角形の位置とサイズを設定
+    rect_x0, rect_y0,  = int(0.7 * w), interval_size,  # 四角形の大きさ（必要に応じて調整）
+    rect_x1, rect_y1 = rect_x0 + month_day_fontsize, rect_y0 + month_day_fontsize
+    radius = 40  # 角の丸み
+    teiki_fontsize = int((rect_x1 - rect_x0) / 2 - 40)
+    teiki_font = ImageFont.truetype(font_path, teiki_fontsize)
+
+
+    # 角丸四角形を背景画像上に描画
+    draw_rounded_rectangle(draw, [rect_x0, rect_y0, rect_x1, rect_y1], radius, 'white')
+
+    teiki_text = "定期\n練習"
+    text_width, text_height = draw.textsize(teiki_text, font=teiki_font)
+    teiki_interval = 10
+    text_x = (rect_x0 + rect_x1 - text_width - teiki_interval) / 2
+    text_y = (rect_y0 + rect_y1 - text_height - teiki_interval) / 2 - 10  # 少し上に調整
+    draw.text((text_x, text_y), "定", fill="red", font=teiki_font)
+    draw.text((text_x + teiki_fontsize + teiki_interval, text_y), "期", fill="red", font=teiki_font)
+    draw.text((text_x, text_y + teiki_fontsize + teiki_interval), "練", fill="red", font=teiki_font)
+    draw.text((text_x + teiki_fontsize + teiki_interval, text_y + teiki_fontsize + teiki_interval), "習", fill="red", font=teiki_font)
 
     # Save the edited image
     image_name = f'practice_{data_id}.jpg'
